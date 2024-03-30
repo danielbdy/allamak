9d2feff1d0d0af2fd3af6c54d25a9fad3f5a7564
hweilee <hweileeong@gmail.com>
2024-03-30 11:44:42 +0800

    test

--- ingest.py
+++ ingest.py
@@ -1,9 +1,8 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
-<<<<<<< HEAD
 DATA_PATH = 'raw_data/'
 DB_FAISS_PATH = 'vectorstores/dbfaiss'
 
@@ -19,23 +18,3 @@ def create_vector_db():
   db.save_local(DB_FAISS_PATH)
 if __name__ == '__main__':
   create_vector_db()
-=======
-DATA_PATH = "data/"
-DB_FAISS_PATH = "vectorstores/db_faiss"
-
-# create vector database
-def create_vector_db():
-    loader = DirectoryLoader(DATA_PATH, glob="*.pdf", loader_cls=PyPDFLoader)
-    documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
-    texts = text_splitter.split_documents(documents)
-
-    embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2',
-                                       model_kwargs = {'device':'cuda'})
-
-    db = FAISS.from_documents(texts, embeddings)
-    db.save_local(DB_FAISS_PATH)
-
-if __name__ == '__main__':
-    create_vector_db()
->>>>>>> origin/main
