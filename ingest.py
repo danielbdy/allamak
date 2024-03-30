17db98c686981755b6c509a31094c1eed7447275
hweilee <hweileeong@gmail.com>
2024-03-30 15:09:18 +0800

    modified

--- ingest.py
+++ ingest.py
@@ -6,29 +6,37 @@ from data import get_data_from_bucket
 import os
 
 # Specify the file name you want to retrieve from the bucket
+# DATA_PATH = "raw_data/"
+
 file_name = "71763-gale-encyclopedia-of-medicine.-vol.-1.-2nd-ed.pdf"
 
 # Call the function to retrieve data from the bucket
 data = get_data_from_bucket(file_name)
+
+print(data)
 
 # Define the local directory to save the downloaded file
 # LOCAL_DATA_PATH = 'raw_data/'
 
 
 # Define the directory where the vector database will be saved
-DB_FAISS_PATH = 'vectorstores/dbfaiss'
+# DB_FAISS_PATH = 'vectorstores/dbfaiss'
 
-def create_vector_db():
-    loader = DirectoryLoader('.', glob='*.pdf', loader_cls=PyPDFLoader)
-    documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap =50)
-    texts = text_splitter.split_documents(documents)
+# def create_vector_db():
+#     # loader = DirectoryLoader(DATA_PATH, glob = '*pdf', loader_cls=PyPDFLoader)
+#     loader = DirectoryLoader('.', glob='*.pdf', loader_cls=PyPDFLoader)
+#     documents = loader.load()
+#     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap =50)
+#     texts = text_splitter.split_documents(documents)
 
-    embeddings = HuggingFaceEmbeddings( model_name =  'sentence-transformers/all-MiniLM-L6-v2',
-                                       model_kwargs = {'device' : 'cpu'})
-    db = FAISS.from_documents(texts, embeddings)
-    db.save_local(DB_FAISS_PATH)
+#     embeddings = HuggingFaceEmbeddings( model_name =  'sentence-transformers/all-MiniLM-L6-v2',
+#                                        model_kwargs = {'device' : 'cpu'})
+#     db = FAISS.from_documents(texts, embeddings)
+#     db.save_local(DB_FAISS_PATH)
 
 
-if __name__ == '__main__':
-    create_vector_db()
+
+
+
+# if __name__ == '__main__':
+#     create_vector_db()
