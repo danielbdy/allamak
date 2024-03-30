655936a7097d302a49e9cd6247fce5c739d6cdd7
hweilee <hweileeong@gmail.com>
2024-03-30 14:31:38 +0800

    modified

--- ingest.py
+++ ingest.py
@@ -19,15 +19,16 @@ data = get_data_from_bucket(file_name)
 DB_FAISS_PATH = 'vectorstores/dbfaiss'
 
 def create_vector_db():
-
-    loader = PyPDFLoader(data)
+    loader = DirectoryLoader('.', glob='*.pdf', loader_cls=PyPDFLoader)
     documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap =50)
     texts = text_splitter.split_documents(documents)
 
-    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2', model_kwargs={'device': 'cpu'})
+    embeddings = HuggingFaceEmbeddings( model_name =  'sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs = {'device' : 'cpu'})
     db = FAISS.from_documents(texts, embeddings)
     db.save_local(DB_FAISS_PATH)
+
 
 if __name__ == '__main__':
     create_vector_db()
